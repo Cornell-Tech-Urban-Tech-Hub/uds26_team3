c858ac91641e0c37f3db475d2319c904e1dd5cac
Yixuan Liu <166225919+ll14han@users.noreply.github.com>
2026-04-29 18:30:35 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", 
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/uds26_team3",
+  assetPrefix: "/uds26_team3/",
 };
 
 export default nextConfig;
